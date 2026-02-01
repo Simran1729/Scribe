1db161c2bcf98badec8a38389eac4f1dce0a5e96
Simran <bhatnagarsimran1306@gmail.com>
2026-02-02 00:17:44 +0530

    init

---
+++ script.ts
@@ -0,0 +1,21 @@
+import {prisma} from './lib/prisma'
+
+async function main() {
+    const user = await prisma.user.create({
+        data : {
+            name : "Simran",
+            email : "simran@yopmail.com"
+        }
+    })
+    console.log("Created user : ", user);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
