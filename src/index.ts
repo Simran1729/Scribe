fe1c7d8b77c2b7dbf98bc5de599ba18b268ef509
Simran <bhatnagarsimran1306@gmail.com>
2026-02-19 23:50:38 +0530

    data modeling in progress

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import {prisma} from './lib/prisma'
+import { prisma } from "./lib/prisma"
 
 async function main() {
     const user = await prisma.user.create({
         data : {
-            name : "Simran",
+            name : "Taman",
             email : "simran@yopmail.com"
         }
     })
