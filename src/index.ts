415a5535697ee14352e42af6c9f7f5a3e3fe8f57
Simran <bhatnagarsimran1306@gmail.com>
2026-02-25 00:10:49 +0530

    init : feature based folder structure

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import { prisma } from "./lib/prisma"
+// import { prisma } from "./lib/prisma"
 
-async function main() {
-    const user = await prisma.user.create({
-        data : {
-            name : "Taman",
-            email : "simran@yopmail.com"
-        }
-    })
-    console.log("Created user : ", user);
-}
+// async function main() {
+//     const user = await prisma.user.create({
+//         data : {
+//             name : "Taman",
+//             email : "simran@yopmail.com"
+//         }
+//     })
+//     console.log("Created user : ", user);
+// }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect()
+//   })
+//   .catch(async (e) => {
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+//   })
